c0bd10de18c0d03c075329dc5857629eb9fe2a90
Jyothish Varghese <jyothish.j.varghese@gmail.com>
2018-09-20 22:58:51 -0400

    Version22

--- LondonBridgeDefender/Assets/Scripts/Player.cs
+++ LondonBridgeDefender/Assets/Scripts/Player.cs
@@ -2,15 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    protected int speed;
+    [SerializeField]
+    protected int jumpForce;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private Rigidbody2D _rigid;
+    private PlayerAnimation _anim;
+
+    private bool isGrounded;
+    private bool resetJumpNeeded;
+
+    public void Start()
+    {
+        _rigid = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<PlayerAnimation>();
+    }
+
+    public void Update()
+    {
+        float transition = Input.GetAxis("Horizontal");
+        _rigid.velocity = new Vector2(transition * speed, _rigid.velocity.y);
+        _anim.Run(transition);
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, 1 << 8);
+        Debug.DrawRay(transform.position, Vector2.down * 0.6f, Color.green);
+
+        if(hitInfo != null)
+        {
+            isGrounded = true;
+            resetJumpNeeded = true;
+
+            if (resetJumpNeeded == false && isGrounded == true)
+            {
+                _rigid.velocity = new Vector2(transition * speed, jumpForce);
+                isGrounded = false; 
+            }
+            StartCoroutine(ResetJumpRoutine());
+
+
+        }
+        
+    }
+    IEnumerator ResetJumpRoutine()
+    {
+        yield return new WaitForSeconds(0.5f);
+        resetJumpNeeded = false;
+    }
+    
 }
---
+++ LondonBridgeDefender/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator _anim;
+    private SpriteRenderer _sprite;
+
+    private void Start()
+    {
+        _anim = GetComponentInChildren<Animator>();
+        _sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    public void Run(float move)
+    {
+        _anim.SetFloat("Move", Mathf.Abs(move));
+        Flip(move);
+    }
+    private void Flip(float move)
+    {
+        if (move < 0)
+            _sprite.flipX = true;
+        else if (move > 0)
+            _sprite.flipX = false;
+    }
+}
